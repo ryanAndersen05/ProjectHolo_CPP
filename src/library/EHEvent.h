d0d5fbd5184f15893a4f29a3cd934d74a24e059f
Ryan Andersen <ryan.andersen05@gmail.com>
2026-08-19 03:13:36 -0400

    Adding a delegate system to engine

--- src/library/EHEvent.h
+++ src/library/EHEvent.h
@@ -1,8 +1,48 @@
 ﻿#pragma once
+#include <cstdint>
 #include <functional>
+#include <bits/fs_fwd.h>
+
 
 template<typename... Args>
 class EHAction {
 public:
     using Callback = std::function<void(Args...)>;
+private:
+    struct FCallbackEntry {
+        uint32_t id;
+        Callback callback;
+    };
+
+public:
+
+
+    EHAction() : nextId(0) {}
+
+    uint32_t AddListener(Callback callback) {
+        int newId = nextId++;
+        callbacks.push_back(
+            {
+                newId,
+                std::move(callback)
+            });
+        return newId;
+    }
+
+    void RemoveListener(uint32_t id) {
+        std::erase_if(callbacks,
+            [id](const FCallbackEntry entry) {
+               return entry.id == id;
+            });
+    }
+
+    void Invoke(Args... args) {
+        for (auto& callbackEntry : callbacks) {
+            callbackEntry.callback(args...);
+        }
+    }
+
+private:
+    uint32_t nextId;
+    std::vector<FCallbackEntry> callbacks;
 };
