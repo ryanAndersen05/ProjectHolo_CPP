85be886735132c87fd24f6f21d8ec0f8112ebd7c
Ryan Andersen <ryan.andersen05@gmail.com>
2026-08-26 01:53:31 -0400

    adding character table and starting input logic

---
+++ src/datatable/EHCharacterTableRow.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include "EHDataTable.h"
+
+class EHCharacterTableRow : public EHDataTableRow {
+private:
+    FName assetId;
+    std::vector<FName> colorPalettes;
+
+    NLOHMANN_DEFINE_DERIVED_TYPE_INTRUSIVE(EHCharacterTableRow, EHDataTableRow, assetId, colorPalettes);
+};
